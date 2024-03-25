25fbbe506c244c9a67b877b33fdba63b73b4c14d
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-03-26 00:27:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// ASSIGNMENT OPERATORS
+let x = 70;
+let y = 20;
+x += y;
+console.log(x);
+y += y;
+console.log(y);
+x += y; // x = x+y
+y += x; // y =x+y
+console.log(y);
+//STRING OPERATORS
+var msg = "HELLO" + "WORLD";
+console.log(msg);
+export {};
---
+++ main.ts
@@ -0,0 +1,22 @@
+    // ASSIGNMENT OPERATORS
+
+let x:number =70;
+let y:number = 20;
+
+x += y
+console.log(x)
+
+y += y
+console.log(y)
+
+x += y   // x = x+y
+y +=x   // y =x+y
+
+console.log(y)
+
+
+  //STRING OPERATORS
+
+var msg:string ="HELLO"+"WORLD";
+
+console.log(msg)
